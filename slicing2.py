4582008294def09e31e39081375c6e6af6df12be
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-07 11:23:39 +0530

    Add files via upload

---
+++ slicing2.py
@@ -0,0 +1,21 @@
+# sum of first and last number
+# a='1,2,3,4,5,6'
+# print(int(a[0])+int(a[-1]))
+# a='python'
+# print(a[::-1])
+# print(a[::1])
+# print(a[::2])
+# print(a[::3])
+# print(a[::4])
+# print(a[::5])
+# print(a[2::2])
+
+# a='python'
+# print(a[1::5])
+# print(a[2::2])
+# print(a[1::2])
+d='python'
+print(d[:5:])
+print(d[1:4:2])
+print(d[4:5:1])
+print(d[5:3:1])
